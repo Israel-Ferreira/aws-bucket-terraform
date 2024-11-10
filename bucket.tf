5d34b496cec59cb9cf3bef6177cbe5440068bea1
Israel-Ferreira <israelfsouza@hotmail.com>
2024-11-10 19:30:26 -0300

    Criando um bucket via Terraform

---
+++ bucket.tf
@@ -0,0 +1,7 @@
+resource "aws_s3_bucket" "bucket-terraform-1706" {
+  bucket = "bucket-terraform-1706"
+}
+
+
+
+ 
---
+++ main.tf
@@ -0,0 +1,21 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 5.0.0"
+    }
+  }
+}
+
+
+provider "aws" {
+  region = "us-east-1"
+
+  default_tags {
+    tags = {
+      owner      = "israelfsouza"
+      managed-by = "terraform"
+    }
+  }
+}
+
